73388d043b960a32f4739993795665b399741f7d
Jeremy Vaughn <itsjbreezyy97@outlookcom>
2022-12-12 20:54:29 -0600

    update

---
+++ challenge4.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#mkdir dir0
+#mkdir dir1
+#mkdir dir2
+#mkdir dir3
+
+files=("dir0" "dir1" "dir2" "dir3")
+
+touch ${files[0]}/text.txt
+touch ${files[1]}/text.txt
+touch ${files[2]}/text.txt
+touch ${files[3]}/text.txt
